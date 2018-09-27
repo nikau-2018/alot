7f1b00f826598f6fb18bb23120f12d0f8c7c50d7
georgehardy <george.hardy@gmail.com>
2018-09-27 18:10:07 +1200

    rename

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,7 @@ const wrapIdentifier = (identifier, origImpl) => origImpl(camelToSnake(identifie
 
 module.exports = {
 
-  _development: {
+  development: {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
@@ -25,7 +25,7 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  development: {
+  staging: {
     client: 'postgresql',
     connection: {
       host: '192.168.1.183',
