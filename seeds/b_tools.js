ad7ecc1fa6ca66e4ba7d48ef4a5c5edbb40e9be1
Aimee Coxhead <aimeecoxhead1@gmail.com>
2018-09-27 16:03:58 +1200

    fixed tools seed data

--- seeds/b_tools.js
+++ seeds/b_tools.js
@@ -6,7 +6,6 @@ exports.seed = function (knex, Promise) {
         {id: 55501, category_id: 77702, name: 'Lawn Mower', description: 'Ryobi 190cc Self-Propelled Yamaha Lawn Mower, powerful 190cc 4-stroke OHV Yamaha engine, self-propelled drive, mulching and side-discharge options, 460mm cutting path with adjustable cutting height, 4 swing back blades for increased cut and safety', image: '/placeholder-image.jpeg', active: true},
         {id: 55502, category_id: 77703, name: 'Paint Brush', description: 'Haydn Synthetic Paint Brush, 100mm wide, ergonomically designed beaver tail handle, suitable for fence painting', image: '/placeholder-image.jpeg', active: true},
         {id: 55503, category_id: 77701, name: 'Sewing Machine', description: 'Brother GS2510 Sewing Machine, 25 built-in stitches, built-in needle threader, automatic button hole, LED light, top load bobbin', image: '/placeholder-image.jpeg', active: true},
-        {id: 55504, category_id: 77704, name: 'Hammer', description: 'Stanley Hammer 565g, fiberglass shaft absorbs shock and vibration, textured rubber comfort grip', image: '/placeholder-image.jpeg', active: true}, 
         {id: 55504, category_id: 77704, name: 'Hammer', description: 'Stanley Hammer 565g, fiberglass shaft absorbs shock and vibration, textured rubber comfort grip', image: '/placeholder-image.jpeg', active: true},
         {id: 55505, category_id: 77702, name: 'Garden Hand Tools', description: 'Fiskars Garden Hand Tool Set 3pc, soft grip', image: '/placeholder-image.jpeg', active: true}
       ])
