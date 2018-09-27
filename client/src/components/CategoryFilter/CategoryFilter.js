28a0187450800055c2d2b204fc9f29691976e1f5
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-09-27 20:33:56 +1200

    Added filterCategory to CategoryFilter component

--- client/src/components/CategoryFilter/CategoryFilter.js
+++ client/src/components/CategoryFilter/CategoryFilter.js
@@ -6,19 +6,12 @@ export default class CategoryFilter extends Component {
     this.props.fetchCategories()
   }
 
-  // The below needs to be added into Tools Container and then passed down to CategoryFilter as props. Check to see if onClick in Dropdown.Item render is working.
-  // onClick (e) {
-  //   this.setState({
-  //     category: e.target.text
-  //   })
-  // }
-
   render () {
     return (
       <Dropdown className= 'categories' text='Categories'>
         <Dropdown.Menu>
           {this.props.categories.map(category =>
-            <Dropdown.Item text={`${category.name}`} key={category.id} onClick={this.props.onClick} />
+            <Dropdown.Item text={`${category.name}`} key={category.id} onClick={this.props.filterCategory(category.id)} />
           )}
         </Dropdown.Menu>
       </Dropdown>
