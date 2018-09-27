d9ac30b51af723e63bff564e9d00d1c56a5f487a
Aimee Coxhead <aimeecoxhead1@gmail.com>
2018-09-27 18:32:38 +1200

    added routes

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,14 +2,21 @@ import React, {Component} from 'react'
 import {Route} from 'react-router-dom'
 
 import './styles.css'
-import Nav from '../Nav'
+import WelcomeContainer from '../WelcomeContainer'
+import ToolsContainer from '../ToolsContainer'
+import ToolDetailContainer from '../ToolDetailContainer'
+import WorkshopsContainer from '../WorkshopsContainer'
+import WorkshopDetailContainer from '../WorkshopDetailContainer'
 
 export default class App extends Component {
   render () {
     return (
       <div className="App">
-        <Nav />
-        Yup. It&lsquo;s an app.
+        <Route exact path='/' component={WelcomeContainer}/>
+        <Route exact path='/tools' component={ToolsContainer}/>
+        <Route path='/tools/:id' component={ToolDetailContainer}/>
+        <Route exact path='/workshops' component={WorkshopsContainer} />
+        <Route path='/workshops/:id' component={WorkshopDetailContainer} />
       </div>
     )
   }
