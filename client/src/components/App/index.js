e1c6e0576986de8e7ecb685d59091ce0c5633575
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-09-27 16:25:12 +1200

    Added CategoryFilter component to App to view in localhost

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -3,12 +3,14 @@ import {Route} from 'react-router-dom'
 
 import './styles.css'
 import Nav from '../Nav'
+import CategoryFilter from '../CategoryFilter'
 
 export default class App extends Component {
   render () {
     return (
       <div className="App">
         <Nav />
+        <CategoryFilter />
         Yup. It&lsquo;s an app.
       </div>
     )
---
+++ client/src/components/CategoryFilter/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import {Button} from 'semantic-ui-react'
+
+export default class Nav extends Component {
+  render () {
+    return (
+      <div>
+        <Button>Search</Button>
+      </div>
+    )
+  }
+}
