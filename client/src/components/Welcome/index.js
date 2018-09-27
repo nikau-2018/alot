9fbe04b904090c381ff1710668fbcf86341685fa
Aimee Coxhead <aimeecoxhead1@gmail.com>
2018-09-27 20:18:32 +1200

    created a welcome component

--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
 
+import logo from '/alot-logo.png'
+
 export default class Footer extends Component {
   render () {
     return (
       <div>
+        <img className='logo' src= {logo}/>
+        <div>
+          <h1>Welcome!</h1>
+        </div>
+        <div>
+          <p>The Auckland Library of tools provides mambers with access to high quality and well maintained tools and equipment.</p>
+        </div>
       </div>
     )
   }
