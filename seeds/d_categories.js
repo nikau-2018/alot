828b846c76da7764ba078908339d9b6a97235868
Aimee Coxhead <aimeecoxhead1@gmail.com>
2018-09-27 15:57:00 +1200

    seed data competed

--- seeds/d_categories.js
+++ seeds/d_categories.js
@@ -2,10 +2,10 @@ exports.seed = function (knex, Promise) {
   return knex('categories').del()
     .then(function () {
       return knex('categories').insert([
-        {id: 77701, name: 'Sewing', description: '', image: '/placeholder-image.jpeg'},
-        {id: 77702, name: 'Gardening', description: '', image: '/placeholder-image.jpeg'},
-        {id: 77703, name: 'Painting', description: '', image: '/placeholder-image.jpeg'},
-        {id: 77704, name: 'Woodwork', description: '', image: '/placeholder-image.jpeg'}
+        {id: 77701, name: 'Sewing', description: 'Everything to do with sewing', image: '/placeholder-image.jpeg'},
+        {id: 77702, name: 'Gardening', description: 'Everything to do with gardening', image: '/placeholder-image.jpeg'},
+        {id: 77703, name: 'Painting', description: 'Everything to do with painting', image: '/placeholder-image.jpeg'},
+        {id: 77704, name: 'Woodwork', description: 'Everything to do with woodwork', image: '/placeholder-image.jpeg'}
       ])
     })
 }
