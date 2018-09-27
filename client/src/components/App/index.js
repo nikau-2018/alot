7da8fe1f641aae9ddf66381dc2ffbbf149c6ab90
Aimee Coxhead <aimeecoxhead1@gmail.com>
2018-09-27 19:05:17 +1200

    commented out routes

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,21 +2,22 @@ import React, {Component} from 'react'
 import {Route} from 'react-router-dom'
 
 import './styles.css'
-import WelcomeContainer from '../WelcomeContainer'
+/* import WelcomeContainer from '../WelcomeContainer'
 import ToolsContainer from '../ToolsContainer'
 import ToolDetailContainer from '../ToolDetailContainer'
 import WorkshopsContainer from '../WorkshopsContainer'
-import WorkshopDetailContainer from '../WorkshopDetailContainer'
+import WorkshopDetailContainer from '../WorkshopDetailContainer' */
 
 export default class App extends Component {
   render () {
     return (
-      <div className="App">
-        <Route exact path='/' component={WelcomeContainer}/>
+      <div className="App" >
+        <h1>We Are Awesome!</h1>
+        {/*   <Route exact path='/' component={WelcomeContainer}/>
         <Route exact path='/tools' component={ToolsContainer}/>
         <Route path='/tools/:id' component={ToolDetailContainer}/>
         <Route exact path='/workshops' component={WorkshopsContainer} />
-        <Route path='/workshops/:id' component={WorkshopDetailContainer} />
+        <Route path='/workshops/:id' component={WorkshopDetailContainer} /> */}
       </div>
     )
   }
