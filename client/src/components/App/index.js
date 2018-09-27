61e24ae9f7ac88fcaaefe47f8783f932d529d9f5
james-built <jgbuilt@gmail.com>
2018-09-27 15:48:41 +1200

    Nav component done with conditional render of log out, will depend on state

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import React, {Component} from 'react'
+import {Route} from 'react-router-dom'
 
 import './styles.css'
+import Nav from '../Nav'
 
 export default class App extends Component {
   render () {
     return (
       <div className="App">
+        <Nav />
         Yup. It&lsquo;s an app.
       </div>
     )
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react'
 import {Button} from 'semantic-ui-react'
-
+import {Link} from 'react-router-dom'
 
 export default class Nav extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      login: true
+    }
+  }
+
   render () {
     return (
-      <div>
-        <p>test</p>
-        <Button></Button>
+      <div className='ui-grid'>
+        <Button.Group vertical>
+          <Link to="/"><Button>Home</Button></Link>
+          <Link to="/tools"><Button>Tools</Button></Link>
+          <Link to="/workshops"><Button>Workshops</Button></Link>
+          <Link to="/register"><Button>Register</Button></Link>
+          {this.state.login
+            ? <Button>Log Out</Button> // these will dispatch actions
+            : <Button>Log In</Button> // these will dispatch actions
+          }
+        </Button.Group>
       </div>
     )
   }
