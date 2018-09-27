3d3d3f13d24e947c7fb430abc7dd5f4dec116f8d
james-built <jgbuilt@gmail.com>
2018-09-27 20:15:09 +1200

    creating map for toolcards based on selected category

--- client/src/components/ToolsContainer/ToolsContainer.js
+++ client/src/components/ToolsContainer/ToolsContainer.js
@@ -4,7 +4,10 @@ export default class ToolsContainer extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      displayedCategory: ''
+      displayedCategory: [
+        'Sewing', 'Gardening', 'Painting', 'Woodwork'
+      ],// will be set from the store in redux
+      Tools: []
     }
   }
 
@@ -13,5 +16,17 @@ export default class ToolsContainer extends Component {
       displayedCategory: selectedCategory
     })
   }
-  
+
+  render() {
+    displayedCategory.map((category) => {
+      <ToolCard
+      key={}
+      name={}
+      image={}
+      description={}/>
+    })
+    return (
+
+    )
+  }
 }
