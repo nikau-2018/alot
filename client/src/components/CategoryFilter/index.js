5730e780352bc42eb22535b1646fa73e313cbd50
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-09-27 18:34:02 +1200

    Setting up component

--- client/src/components/CategoryFilter/index.js
+++ client/src/components/CategoryFilter/index.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react'
 import {Dropdown} from 'semantic-ui-react'
 
-export default class CategoryFilter extends Component {
-  componentDidMount () {
+// import {fetchPosts} from '../../actions'
 
+export default class CategoryFilter extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+}
+  componentDidMount () {
+    // this.props.dispatch(fetchPosts())
   }
 
   render () {
     return (
-      <Dropdown text='Categories'>
+      <Dropdown className= 'categories' text='Categories'>
         <Dropdown.Menu>
           <Dropdown.Item text='Gardening' />
           <Dropdown.Item text='Cooking' />
