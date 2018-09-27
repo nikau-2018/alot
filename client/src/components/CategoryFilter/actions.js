b7f1bda1dd24db63c80ce81460c9394de02ce91f
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-09-27 18:58:17 +1200

    Creating actions and reducer

--- client/src/components/CategoryFilter/actions.js
+++ client/src/components/CategoryFilter/actions.js
@@ -1,1 +1,41 @@
 import request from 'axios'
+
+export const SHOW_ERROR = 'SHOW_ERROR'
+export const RECEIVE_CATEGORIES = 'RECEIVE_CATEGORIES'
+export const REQUEST_CATEGORIES = 'REQUEST_CATEGORIES'
+
+export const showError = (errorMessage) => {
+  return {
+    type: SHOW_ERROR,
+    errorMessage: errorMessage
+  }
+}
+
+export const requestCategories = () => {
+  return {
+    type: REQUEST_CATEGORIES
+  }
+}
+
+export const receiveCategories = (categories) => {
+  return {
+    type: RECEIVE_CATEGORIES,
+    categories: categories
+  }
+}
+
+export function fetchCategories () {
+  return (dispatch) => {
+    dispatch(requestCategories())
+    return request
+      .get('/api/v1/categories')
+      // IS THE BELOW NEEDED INSTEAD?
+      // .get('/api/v1/categories', {headers: getHeaders()})      
+      .then(res => {
+        dispatch(receiveCategories(res.data.categories))
+      })
+      .catch(err => {
+        dispatch(showError(err.message))
+      })
+  }
+}
