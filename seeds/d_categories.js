a2521bd78ff81ef0e02f3699d27eeabc72b4ca6a
Aimee Coxhead <aimeecoxhead1@gmail.com>
2018-09-27 12:59:19 +1200

    created categories seed data

--- seeds/d_categories.js
+++ seeds/d_categories.js
@@ -1,13 +1,11 @@
-
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+exports.seed = function (knex, Promise) {
+  return knex('categories').del()
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+      return knex('categories').insert([
+        {id: 77701, name: 'Sewing', description: '', image: '/placeholder-image.jpeg'},
+        {id: 77702, name: 'Gardening', description: '', image: '/placeholder-image.jpeg'},
+        {id: 77703, name: 'Painting', description: '', image: '/placeholder-image.jpeg'},
+        {id: 77704, name: 'Hand Tools', description: '', image: '/placeholder-image.jpeg'}
+      ])
+    })
+}
