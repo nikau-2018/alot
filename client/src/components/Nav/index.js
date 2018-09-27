a54e56484f0a400009ea049a84b870e7227a10d7
james-built <jgbuilt@gmail.com>
2018-09-27 12:15:33 +1200

    working on nav

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+export default class Nav extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Tools</p>
+      </div>
+    )
+  }
+}
