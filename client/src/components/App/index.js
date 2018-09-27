74c7318de180d4331b3763f51b8077eadff0e851
james-built <jgbuilt@gmail.com>
2018-09-27 14:04:44 +1200

    added route to App

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 
 import './styles.css'
 
 export default class App extends Component {
   render () {
     return (
-      <div className="main">
+      <div className="App">
         Yup. It&lsquo;s an app.
       </div>
     )
