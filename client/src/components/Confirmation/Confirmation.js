5f3848a7ba57c5586c1a5aed9ed57d918870b27f
james-built <jgbuilt@gmail.com>
2018-09-28 18:21:33 +1200

    stored tool ID from params & filter tools from store to get selected tool

--- client/src/components/Confirmation/Confirmation.js
+++ client/src/components/Confirmation/Confirmation.js
@@ -2,13 +2,13 @@ import React, {Component} from 'react'
 import {Container} from 'semantic-ui-react'
 
 export default class Confirmation extends Component {
-  
-  render () {
+  render (props) {
+    const toolId = Number(props.match.params.id)
+    const selectedTool = props.tools.filter((tool) => toolId === tool.id)
     return (
       <Container>
         <div className='confirmation'>
-          <p>Are you sure you would like to rent ?</p>
-          
+          <p>Are you sure you would like to rent {selectedTool.name}?</p>
         </div>
       </Container>
     )
