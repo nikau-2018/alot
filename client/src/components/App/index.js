876a7ea5184bd203ca0c254889906cd8dad3db2a
james-built <jgbuilt@gmail.com>
2018-09-28 16:17:38 +1200

    working on the confirmation page for tool rental

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -6,25 +6,25 @@ import './styles.css'
 // components for testing, remove later
 import Nav from '../Nav'
 // import WelcomeContainer from '../WelcomeContainer'
-import ToolsContainer from '../ToolsContainer'
+// import ToolsContainer from '../ToolsContainer'
 /* import ToolDetailContainer from '../ToolDetailContainer'
 import WorkshopsContainer from '../WorkshopsContainer'
 import WorkshopDetailContainer from '../WorkshopDetailContainer' */
+import Confirmation from '../Confirmation'
 
 export default class App extends Component {
   render () {
     return (
       <div className="app">
         <Nav />
-        Yup. It&lsquo;s an app.
-        <ToolsContainer />
+        {/* <ToolsContainer /> */}
         {/* <Route exact path='/' component={WelcomeContainer}/> */}
-        <h1>We Are Awesome!</h1>
         {/*   <Route exact path='/' component={WelcomeContainer}/>
         <Route exact path='/tools' component={ToolsContainer}/>
         <Route path='/tools/:id' component={ToolDetailContainer}/>
         <Route exact path='/workshops' component={WorkshopsContainer} />
         <Route path='/workshops/:id' component={WorkshopDetailContainer} /> */}
+        <Route exact path='/confirmation' component={Confirmation} />
       </div>
     )
   }
---
+++ client/src/components/Confirmation/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Container} from 'semantic-ui-react'
+
+const Confirmation = () => (
+  <Container>
+    <div className='confirmation'>
+      <p>test</p>
+    </div>
+  </Container>
+)
+
+export default Confirmation
