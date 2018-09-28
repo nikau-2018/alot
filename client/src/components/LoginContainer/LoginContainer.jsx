3579f766e7089d1a2955dec4dd7ebeed3bee70f6
georgehardy <george.hardy@gmail.com>
2018-09-28 20:33:39 +1200

    make login form functional

---
+++ client/src/components/LoginContainer/LoginContainer.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import {Button, Form} from 'semantic-ui-react'
+import {Link} from 'react-router-dom'
+
+// import ErrorMessage from './ErrorMessage'
+
+export default class LoginContainer extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: ''
+    }
+    this.handleClick = this.handleClick.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (e) {
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleClick () {
+    const {email, password} = this.state
+    const creds = {
+      email: email.trim(),
+      password: password.trim()
+    }
+    this.props.loginUser(creds)
+  }
+
+  render () {
+    return (
+      <Form>
+        <div className='login'>
+          <h1> Login </h1>
+          <Form.Field>
+            <label>Email</label>
+            <input name='email'
+              placeholder='Email'
+              onChange={this.handleChange} />
+          </Form.Field>
+          <Form.Field>
+            <label>Password</label>
+            <input name='password'
+              type='password'
+              placeholder='Password'
+              onChange={this.handleChange} />
+          </Form.Field>
+          <Button onClick={this.handleClick}>Submit</Button>
+          {/* <ErrorMessage reducer='auth' /> */}
+          <Link to={'/Register'} className='button'>
+            <Button type='Need'>Need an account?</Button> </Link>
+        </div>
+      </Form>
+    )
+  }
+}
