264f92c703d19fd18ee79c08cca615717435c541
Aimee Coxhead <aimeecoxhead1@gmail.com>
2018-09-28 18:32:37 +1200

    made it a stateless component

--- client/src/components/ToolDetail/index.js
+++ client/src/components/ToolDetail/index.js
@@ -1,18 +1,15 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-export default class ToolDetail extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {} // not sure if we will need state yet
-  }
-  render () {
-    return (
-      <div>
-        <h1>{this.props.tool.name}</h1>
-        <img src={this.props.tool.image}/>
-        <h3>Description:</h3>
-        <p>{this.props.tool.description}</p>
-      </div>
-    )
-  }
+import { Divider } from 'semantic-ui-react'
+
+export default function ToolDetail (props) {
+  return (
+    <div>
+      <h1>{props.tool.name}</h1>
+      <Divider/>
+      <img src={props.tool.image}/>
+      <h3>Description:</h3>
+      <p>{props.tool.description}</p>
+    </div>
+  )
 }
