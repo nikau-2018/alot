6106f33bb5b964f9e1269f7c444f5fef75fcc7fc
james-built <jgbuilt@gmail.com>
2018-09-28 14:55:40 +1200

    added db query single tool

--- server/db/tools.js
+++ server/db/tools.js
@@ -1,10 +1,16 @@
 const connection = require('./')
 
 module.exports = {
-  getTools
+  getTools,
+  getSingleTool
 }
 
 function getTools (db = connection) {
   return db('tools')
     .select()
+}
+
+function getSingleTool (db = connection, toolId) {
+  return db('tools')
+    .where('id', toolId)
 }
