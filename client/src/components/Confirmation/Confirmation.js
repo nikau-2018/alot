fc7023a7dbdbc34ca07d95f6438d315f7c991393
james-built <jgbuilt@gmail.com>
2018-09-29 00:25:45 +1200

    removed default props, added IIFE to conditionally render confirmation heading

--- client/src/components/Confirmation/Confirmation.js
+++ client/src/components/Confirmation/Confirmation.js
@@ -6,39 +6,38 @@ export default class Confirmation extends Component {
     const type = (this.props.match.params.type)
     const toolId = Number(this.props.match.params.id)
     const [ selectedTool ] = this.props.tools.filter((tool) => toolId === tool.id)
-    console.log(selectedTool)
+
     return (
       <div className='confirmation'>
-          <h1>Confirm Your Rental</h1>
-          <Divider/>
-          <Item.Image src={selectedTool.image}/>
-            <Item.Header as='a'>{selectedTool.name}</Item.Header>
-            <Item.Meta>
-              Tool subtitle
-            </Item.Meta>
-              {selectedTool.description}
-              <br/>
-              <p>Are you sure you would like to rent this tool?</p>
-              <br/>
-              <p>It will be available for collection from 9am Monday - Friday</p>
-              <Button.Group>
-                <Button>Cancel</Button>
-                <Button.Or />
-                <Button positive>Confirm</Button>
-              </Button.Group>
+      {
+        (() => {
+          switch(type) {
+            case 'tool':
+            return <h1>Confirm Your Tool Rental</h1>
+            case 'workshop':
+            return <h1>Confirm Your Workshop Booking</h1>
+          }
+        })()  // using an IIFE here, not entirely necessary probably but wanted to do it for learning
+      }
+      <Divider/>
+      <Image src={selectedTool.image}/>
+        <Item.Header as='a'>{selectedTool.name}</Item.Header>
+        <Item.Meta>
+          Tool subtitle
+        </Item.Meta>
+          {selectedTool.description}
+          <br/>
+          <p>Are you sure you would like to rent this tool?</p>
+          <br/>
+          <p>It will be available for collection from 9am Monday - Friday</p>
+          <Button.Group>
+            <Button>Cancel</Button>
+            <Button.Or />
+            <Button positive>Confirm</Button>
+          </Button.Group>
 
       </div>
     )
   }
 }
 
-// Confirmation.defaultProps = {
-//   selectedTool: [{
-//     id: 55501,
-//     categoryId: 77702,
-//     name: 'lawn Mower',
-//     description: 'Ryobi 190cc....',
-//     image: '/placeholder-image.jpeg',
-//     active: true
-//   }]
-// }
