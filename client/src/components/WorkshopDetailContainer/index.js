b60990649dd0edb44c7551103e433ee7237d0850
Aimee Coxhead <aimeecoxhead1@gmail.com>
2018-09-28 16:24:25 +1200

    added constructor

--- client/src/components/WorkshopDetailContainer/index.js
+++ client/src/components/WorkshopDetailContainer/index.js
@@ -3,6 +3,14 @@ import React, { Component } from 'react'
 import WorkshopDetail from '../WorkshopDetail'
 
 export default class WorkshopDetailContainer extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      workshop: {}
+    }
+  }
+
+
   render () {
     return (
       <div className='workshop-detail-container' >
