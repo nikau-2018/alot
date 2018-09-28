996fe88e63e14c1c11a8bc28e5282352525aac37
Aimee Coxhead <aimeecoxhead1@gmail.com>
2018-09-28 18:18:50 +1200

    tool detail content done

--- client/src/components/ToolDetail/index.js
+++ client/src/components/ToolDetail/index.js
@@ -5,17 +5,14 @@ export default class ToolDetail extends Component {
     super(props)
     this.state = {} // not sure if we will need state yet
   }
-
   render () {
     return (
       <div>
         <h1>{this.props.tool.name}</h1>
         <img src={this.props.tool.image}/>
-        <h5></h5>
+        <h3>Description:</h3>
         <p>{this.props.tool.description}</p>
       </div>
     )
   }
 }
-
-
