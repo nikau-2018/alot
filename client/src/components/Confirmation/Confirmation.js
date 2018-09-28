1eca4aaff0664b63352cb2eda58fc07c7c427153
james-built <jgbuilt@gmail.com>
2018-09-28 18:44:38 +1200

    mock up of the confirmation view

--- client/src/components/Confirmation/Confirmation.js
+++ client/src/components/Confirmation/Confirmation.js
@@ -1,16 +1,33 @@
 import React, {Component} from 'react'
-import {Container} from 'semantic-ui-react'
+import {Button, Icon, Item, Label, Image} from 'semantic-ui-react'
 
 export default class Confirmation extends Component {
   render (props) {
     const toolId = Number(props.match.params.id)
     const selectedTool = props.tools.filter((tool) => toolId === tool.id)
     return (
-      <Container>
-        <div className='confirmation'>
-          <p>Are you sure you would like to rent {selectedTool.name}?</p>
-        </div>
-      </Container>
+      <div className='confirmation'>
+        <Item>
+          <Item.Image src=''/>
+          <Item.Content>
+            <Item.Header as='a'>{selectedTool.name}</Item.Header>
+            <Item.Meta>
+              Tool subtitle
+            </Item.Meta>
+            <Item.Description>
+              {selectedTool.description}
+              <p>Are you sure you would like to rent this tool?</p>
+            </Item.Description>
+            <Item.Extra>
+              <Button.Group>
+                <Button>Cancel</Button>
+                <Button.Or />
+                <Button positive>Confirm</Button>
+              </Button.Group>
+            </Item.Extra>
+          </Item.Content>
+        </Item>
+      </div>
     )
   }
 }
