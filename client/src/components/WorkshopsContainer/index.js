1a84c9884283229997b11d3e824d228c43d077c8
james-built <jgbuilt@gmail.com>
2018-09-29 11:38:56 +1200

    removed categories map to props from the workshopscontainer connection

--- client/src/components/WorkshopsContainer/index.js
+++ client/src/components/WorkshopsContainer/index.js
@@ -1,21 +1,17 @@
 import {connect} from 'react-redux'
 
 import {fetchWorkshops} from './actions'
-import {fetchCategories} from '../CategoryFilter/actions'
 
 import WorkshopsContainer from './WorkshopsContainer'
 
 const mapStateToProps = (state) => {
-  const allCategories = state.categories.map((category) => category.id)
   return {
-    workshops: state.workshops,
-    displayedCategory: allCategories
+    workshops: state.workshops
   }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchWorkshops: () => dispatch(fetchWorkshops()),
-  // fetchCategories: () => dispatch(fetchCategories())
+  fetchWorkshops: () => dispatch(fetchWorkshops())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(WorkshopsContainer)
