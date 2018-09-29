46a77910d2c202f0cfcc9bc7ba1c49aee90cad15
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-09-29 17:14:41 +1200

    Removed unneccesary code

--- client/src/components/ToolsContainer/ToolsContainer.js
+++ client/src/components/ToolsContainer/ToolsContainer.js
@@ -4,31 +4,17 @@ import React, {Component} from 'react'
 import Tools from '../Tools'
 
 export default class ToolsContainer extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      displayedCategory: null
-    }
-  }
-
   componentDidMount () {
     this.props.fetchTools()
-      .then(() => {
-        this.setState({
-          displayedCategory: this.props.displayedCategory
-        })
-      })
+    // .then(() => {
+    //   this.setState({
+    //     displayedCategory: this.props.displayedCategory
+    //   })
+    // })
     this.props.fetchCategories()
   }
 
-  // filterCategory () {
-  //   this.setState({
-  //     displayedCategory: [Number(this.props.match.params.category)]
-  //   })
-  // }
-
   render () {
-    console.log(this.props.match.params.category)
     const filteredTools = this.props.match.params.category == null
       ? this.props.tools
       : this.props.tools.filter((tool) => {
