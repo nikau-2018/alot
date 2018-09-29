9a3a5dafaca818f693a1bf2d36483fe07b574c8f
james-built <jgbuilt@gmail.com>
2018-09-30 00:17:22 +1200

    filter of workshops working based on the parameter passed in

--- client/src/components/WorkshopsContainer/WorkshopsContainer.js
+++ client/src/components/WorkshopsContainer/WorkshopsContainer.js
@@ -9,21 +9,21 @@ export default class WorkshopsContainer extends Component {
   }
   
   render () {
-    const workshopId = this.props.categories.find((cat) => {
-      cat.name == this.props.match.params.category
-    })
-    const filteredWorkshops = this.props.workshops.map((workshop) => (
-      workshop.id === workshopId
+    const {id} = this.props.categories.find((cat) => (
+      cat.name.toLowerCase() == this.props.match.params.category
     ))
-      console.log(this.props.categories)
-      console.log(typeof this.props.match.params.category)
-      console.log(workshopId)
+    const filteredWorkshops = this.props.workshops.filter((workshop) => (
+      workshop.categoryId === id
+    ))
+      console.log(this.props.workshops)
+      console.log(id)
+      console.log(filteredWorkshops)
     return (
       <div className='workshops-container' >
       {
         this.props.match.params.category
         ? <Workshops
-        workshops={this.props.workshops}
+        workshops={filteredWorkshops}
         category={this.props.categories}/>
         : <Workshops
         workshops={this.props.workshops}
