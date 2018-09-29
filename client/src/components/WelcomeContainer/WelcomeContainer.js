5a0e765e956bf3c3248ae38aed6702d1ab0f57a2
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-09-29 20:10:43 +1200

    Added heading

--- client/src/components/WelcomeContainer/WelcomeContainer.js
+++ client/src/components/WelcomeContainer/WelcomeContainer.js
@@ -5,12 +5,12 @@ import ToolCard from '../ToolCard'
 
 export default class WelcomeContainer extends Component {
   render () {
-    const randomTool = this.props.tools[Math.floor(Math.random() * this.props.tools.length)]    
+    const randomTool = this.props.tools[Math.floor(Math.random() * this.props.tools.length)]
     return (
       <div className='welcome-container' >
         <Welcome />
         <hr/>
-        <h3>What a tool test...</h3>        
+        <h3>What a tool test...</h3>
         <ToolCard
           key={randomTool.id}
           id={randomTool.id}
