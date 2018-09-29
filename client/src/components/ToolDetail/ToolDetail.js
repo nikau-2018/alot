bfe99e591ba2245109c10f0e9c8a60f6d74b4736
aimee-coxhead <aimeecoxhead1@gmail.com>
2018-09-29 23:10:59 +1200

    made tool description into a list

--- client/src/components/ToolDetail/ToolDetail.js
+++ client/src/components/ToolDetail/ToolDetail.js
@@ -3,13 +3,14 @@ import {Image, Button, Divider} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
 
 export default function ToolDetail (props) {
+  const description = props.tool.description || ''
   return (
     <div>
       <h1>{props.tool.name}</h1>
       <Divider/>
       <Image src={props.tool.image} size='large' />
       <h3>Description:</h3>
-      <p>{props.tool.description}</p>
+      <ul>{description.split(', ').map(element => <li>{element}</li>)}</ul>
       <Link to={`/confirm/tool/${props.tool.id}`}>
         <Button basic color='green'>Rent</Button>
       </Link>
