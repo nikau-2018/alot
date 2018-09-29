a14d68324252a0be3236a7e5faafc6f612e79e3f
Aimee Coxhead <aimeecoxhead1@gmail.com>
2018-09-29 16:12:05 +1200

    added garden trolley to seed data

--- seeds/c_tools.js
+++ seeds/c_tools.js
@@ -7,7 +7,8 @@ exports.seed = function (knex, Promise) {
         {id: 55503, category_id: 77701, name: 'Sewing Machine', description: 'Brother GS2510 Sewing Machine, 25 built-in stitches, built-in needle threader, automatic button hole, LED light, top load bobbin', image: '/tool-images/sewing-machine.jpg', active: true},
         {id: 55504, category_id: 77704, name: 'Hammer', description: 'Stanley Hammer 565g, fiberglass shaft absorbs shock and vibration, textured rubber comfort grip', image: '/tool-images/hammer.jpg', active: true},
         {id: 55505, category_id: 77702, name: 'Garden Hand Tools', description: 'Fiskars Garden Hand Tool Set 3pc, soft grip', image: '/tool-images/garden-hand-tools.jpg', active: true},
-        {id: 55506, category_id: 77702, name: 'Rake', description: 'Saxon Long Handle 14 Tine Garden Rake, long timber handle, ideal for raking and clearing soils, leaves and stones ', image: '/tool-images/rake.jpg', active: true}
+        {id: 55506, category_id: 77702, name: 'Rake', description: 'Saxon Long Handle 14 Tine Garden Rake, long timber handle, ideal for raking and clearing soils, leaves and stones ', image: '/tool-images/rake.jpg', active: true},
+        {id: 55507, category_id: 77702, name: 'Garden Trolley', description: '60L Garden Trolley, good volume carrying capacity, strong, lightweight', image: '/tool-images/garden-trolley.jpg', active: true}
       ])
     })
 }
