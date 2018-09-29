bb18fec56e2fb479c0f0536761aadd421c8c1efb
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-09-29 16:12:25 +1200

    Fixed error

--- client/src/reducers.js
+++ client/src/reducers.js
@@ -2,7 +2,7 @@ import {combineReducers} from 'redux'
 
 // Import the reducers here from the component file
 // e.g import register from './components/Register/reducer'
-import { categories } from './components/CategoryFilter/reducer'
+import categories from './components/CategoryFilter/reducer'
 import tools from './components/ToolsContainer/reducer'
 import auth from './components/Auth/reducer'
 import workshops from './components/WorkshopsContainer/reducer'
