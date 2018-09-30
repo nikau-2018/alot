ecc7b7bb9422a1f977c9677118b2f2b5583ae573
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-10-01 08:58:07 +1300

    Changed /test to /admin

--- client/src/components/Admin/index.js
+++ client/src/components/Admin/index.js
@@ -4,6 +4,7 @@ import React, {Component} from 'react'
 // import AdminNav from '../AdminNav'
 import Orders from '../Orders'
 
+// Not sure whether this needs to be stateful. Will leave as stateful for now.
 export default class Admin extends Component {
   render () {
     return (
