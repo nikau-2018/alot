0c319d7bd17f233092371a65716be696918de0ee
james-built <jgbuilt@gmail.com>
2018-09-30 15:55:33 +1300

    params filter working for all & for categories in workshops

--- client/src/components/WorkshopsContainer/WorkshopsContainer.js
+++ client/src/components/WorkshopsContainer/WorkshopsContainer.js
@@ -3,29 +3,30 @@ import React, {Component} from 'react'
 import Workshops from '../Workshops'
 
 export default class WorkshopsContainer extends Component {
-
   componentDidMount () {
     this.props.fetchWorkshops()
     this.props.fetchCategories()
   }
 
   render () {
-    if (this.props.ready) {
+    if (this.props.ready && this.props.match.params.category) {
       const {id} = this.props.categories.find((cat) => (
         cat.name.toLowerCase() === this.props.match.params.category))
       const filteredWorkshops = this.props.workshops.filter((workshop) => (
         workshop.categoryId === id))
       return (
         <div className='workshops-container' >
-          {
-            this.props.match.params.category
-              ? <Workshops
-                workshops={filteredWorkshops}
-                category={this.props.categories}/>
-              : <Workshops
-                workshops={this.props.workshops}
-                category={this.props.categories}/>
-          }
+          <Workshops
+            workshops={filteredWorkshops}
+            category={this.props.categories}/>
+        </div>
+      )
+    } else if (this.props.ready) {
+      return (
+        <div className='workshops-container' >
+          <Workshops
+            workshops={this.props.workshops}
+            category={this.props.categories}/>
         </div>
       )
     } else {
