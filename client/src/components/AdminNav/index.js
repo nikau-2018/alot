fa6c19483cb7611ef0760d7d92f1805cbb9c87b4
Aimee Coxhead <aimeecoxhead1@gmail.com>
2018-10-01 11:00:26 +1300

    finished Admin Nav component

--- client/src/components/AdminNav/index.js
+++ client/src/components/AdminNav/index.js
@@ -1,33 +1,26 @@
 
-import React, {Component} from 'react'
+import React from 'react'
 import {Button} from 'semantic-ui-react'
 
-export default class AdminNav extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {} // not sure what state we will need
-  }
-
-  render () {
-    return (
-      <div className='ui buttons'>
-        <Button className='ui button' onClick=''>
-          <i className='fire extinguisher icon'></i>
+export default function AdminNav (props) {
+  return (
+    <div className='ui buttons'>
+      <Button className='ui button' onClick={props.handleClick}>
+        <i className='fire extinguisher icon'></i>
       Add Tool
-        </Button>
-        <Button className='ui button' onClick=''>
-          <i className='calendar alternate icon'></i>
+      </Button>
+      <Button className='ui button' onClick={props.handleClick}>
+        <i className='calendar alternate icon'></i>
       Add Workshop
-        </Button>
-        <Button className='ui button' onClick=''>
-          <i className='user plus icon'></i>
+      </Button>
+      <Button className='ui button' onClick={props.handleClick}>
+        <i className='user plus icon'></i>
       Edit Employee
-        </Button>
-        <Button className='ui button' onClick=''>
-          <i className='envelope icon'></i>
+      </Button>
+      <Button className='ui button' onClick={props.handleClick}>
+        <i className='envelope icon'></i>
       Maintain Orders
-        </Button>
-      </div>
-    )
-  }
+      </Button>
+    </div>
+  )
 }
