be5056c36786639213093e63e035fe03ea3ed292
james-built <jgbuilt@gmail.com>
2018-10-01 09:59:35 +1300

    branch switch commit

--- client/src/components/AddTool/index.js
+++ client/src/components/AddTool/index.js
@@ -1,9 +1,14 @@
 import {connect} from 'react-redux'
 
 import AddTool from './AddTool'
+import {addTool} from './actions'
+
+const mapStateToProps = (state) => {
+  return {}
+}
 
 const mapDispatchToProps = () => ({
-  
+  addTool: () => dispatch(addTool())
 })
 
-export default connect()(AddTool)
+export default connect(mapStateToProps, mapDispatchToProps)(AddTool)
