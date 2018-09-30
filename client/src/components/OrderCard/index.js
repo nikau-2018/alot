a775fcfc23a74eb165ac9d0f1ccaddf4abb7afc4
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-10-01 08:45:55 +1300

    Troubleshooting refactoring into OrderCard

---
+++ client/src/components/OrderCard/index.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react'
+import {Card, Icon} from 'semantic-ui-react'
+import {Link} from 'react-router-dom'
+
+import styles from './styles.css'
+
+export default class Orders extends Component {
+  render () {
+    const order = this.props.order
+    return (
+      <div className='order-card'>
+        <Card>
+          <Card.Content>
+            <Card.Header>
+              <Icon name='dolly flatbed' /> Order #{order.id}<br />
+            </Card.Header>
+            <Card.Meta>
+              <Link to={`/tools/${order.category}/${order.toolId}`}>
+                {order.tool}<br/>
+              </Link>
+            Status: {order.status
+              }</Card.Meta>
+            <Card.Description>
+              <Icon name='user' /> {order.firstName} {order.lastName}<br />
+              <Icon name='mail' /><a href={`mailto:${order.email}`}> {order.email}</a><br />
+              <Icon name='phone' /><a href={`tel:${order.phone}`}> {order.phone}</a><br /><br />
+              <Icon name='pencil' /> {order.notes}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Icon name='calendar alternate outline' /> {order.createdAt}
+          </Card.Content>
+        </Card>
+      </div>
+    )
+  }
+}
--- client/src/components/Orders/Orders.js
+++ client/src/components/Orders/Orders.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
-import {Card, Icon} from 'semantic-ui-react'
+import {Card} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
+
+import OrderCard from '../OrderCard'
 
 export default class Orders extends Component {
   componentDidMount () {
@@ -8,35 +10,15 @@ export default class Orders extends Component {
   }
 
   render () {
-    const orderCards = this.props.orders && this.props.orders.map(order =>
-      <Card key={order.id}>
-        <Card.Content>
-          <Card.Header>
-            <Icon name='dolly flatbed' /> Order #{order.id}<br />
-          </Card.Header>
-          <Card.Meta>
-            <Link to={`/tools/${order.category}/${order.toolId}`}>
-              {order.tool}<br/>
-            </Link>
-            Status: {order.status
-            }</Card.Meta>
-          <Card.Description>
-            <Icon name='user' /> {order.firstName} {order.lastName}<br />
-            <Icon name='mail' /><a href={`mailto:${order.email}`}> {order.email}</a><br />
-            <Icon name='phone' /><a href={`tel:${order.phone}`}> {order.phone}</a><br /><br />
-            <Icon name='pencil' /> {order.notes}
-          </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <Icon name='calendar alternate outline' /> {order.createdAt}
-        </Card.Content>
-      </Card>
+    const orders = this.props.orders
     )
     return (
       <div className='orders'>
         <h2>Orders</h2>
         <Card.Group>
-          {orderCards}
+          {orders && orders.map(order =>
+            <OrderCard key={order.id} order={order}/>
+          )}
         </Card.Group>
       </div>
     )
