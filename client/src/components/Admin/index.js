5b1d8497ae8251531e1ed8d9e97c8de3edb5eb51
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-10-01 09:02:31 +1300

    Commented out children

--- client/src/components/Admin/index.js
+++ client/src/components/Admin/index.js
@@ -3,6 +3,9 @@ import React, {Component} from 'react'
 // Components
 // import AdminNav from '../AdminNav'
 import Orders from '../Orders'
+// import AddTool from '../AddTool'
+// import AddWorkshop from '../AddWorkshop'
+// import AddEmployee from '../AddEmployee'
 
 // Not sure whether this needs to be stateful. Will leave as stateful for now.
 export default class Admin extends Component {
@@ -10,7 +13,11 @@ export default class Admin extends Component {
     return (
       <div className='admin'>
         {/* <AdminNav /> */}
+        {/* Make below children conditionally render based on what's clicked in AdminNav */}
         <Orders />
+        {/* <AddTool /> */}
+        {/* <AddWorkshop /> */}
+        {/* <AddEmployee /> */}
       </div>
     )
   }
