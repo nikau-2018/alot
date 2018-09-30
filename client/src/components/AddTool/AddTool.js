d392ef80eea615ec543afc4b0eb4513c332cf36f
james-built <jgbuilt@gmail.com>
2018-10-01 11:45:58 +1300

    added retrieve token from headers & fetchCategories to pass to form

--- client/src/components/AddTool/AddTool.js
+++ client/src/components/AddTool/AddTool.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import axios from 'axios'
+import {get} from '../Auth/utils/localstorage'
 
 import ToolForm from '../ToolForm'
 
@@ -6,17 +8,26 @@ export default class AddTool extends Component {
   constructor (props) {
     super(props)
     this.state = {
-
+      // could retrieve db table details here which may affect
+      // how the form is rendered, if we want to make form multi purpose
     }
   }
 
-  handleSubmit = (e) {
-    
+  componentDidMount () {
+    this.props.fetchCategories()
+  }
+
+  handleSubmit = (formState) => {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${get('token')}`
+    return axios
+      .post('api/v1/tools/add', formState)
   }
 
   render () {
     return (
-      <ToolForm handleSubmit={this.handleSubmit}/>
+      <ToolForm
+      handleSubmit={this.handleSubmit}
+      categoryIds={props.categoryIds}/>
     )
   }
 }
--- client/src/components/AddTool/index.js
+++ client/src/components/AddTool/index.js
@@ -2,15 +2,18 @@ import {connect} from 'react-redux'
 
 import AddTool from './AddTool'
 import {addTool} from './actions'
+import {fetchCategories} from '../CategoryFilter/actions'
 
 const mapStateToProps = (state) => {
+  const categoryIds = state.categories.categories.map((category) => category.id)
   return {
-    
+    categoryIds: categoryIds
   }
 }
 
-const mapDispatchToProps = () => ({
-  addTool: () => dispatch(addTool())
+const mapDispatchToProps = (dispatch) => ({
+  addTool: () => dispatch(addTool()),
+  fetchCategories: () => dispatch(fetchCategories())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddTool)
