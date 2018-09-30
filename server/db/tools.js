e5aec374fa487849f75313995bf67608aee68b7e
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-10-01 10:46:30 +1300

    Created addTool db function

--- server/db/tools.js
+++ server/db/tools.js
@@ -2,7 +2,9 @@ const connection = require('./')
 
 module.exports = {
   getTools,
-  getSingleTool
+  getSingleTool,
+  addTool,
+  editTool
 }
 
 function getTools (db = connection) {
@@ -14,4 +16,21 @@ function getSingleTool (toolId, db = connection) {
   return db('tools')
     .where('id', toolId)
     .first()
+}
+
+function addTool (tool, db = connection) {
+  return db('tools')
+    .insert({
+      'category_id': tool.categoryId,
+      'name': tool.name,
+      'description': tool.description,
+      'body': tool.body,
+      'image': tool.image,
+      'active': tool.active
+    })
+}
+
+function editTool (db = connection) {
+  return db('tools')
+    .select()
 }
