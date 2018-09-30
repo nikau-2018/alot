3d03a6857c83130398eaed60db93df264b165369
Aimee Coxhead <aimeecoxhead1@gmail.com>
2018-09-30 17:34:16 +1300

    added body to the migration and seed data

--- client/src/components/ToolDetail/ToolDetail.js
+++ client/src/components/ToolDetail/ToolDetail.js
@@ -3,14 +3,15 @@ import {Image, Button, Divider} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
 
 export default function ToolDetail (props) {
-  const description = props.tool.description || ''
+  const body = props.tool.body || ''
   return (
     <div>
       <h1>{props.tool.name}</h1>
       <Divider/>
       <Image src={props.tool.image} size='large' />
-      <h3>Description:</h3>
-      <ul>{description.split('*').map(element => <li>{element}</li>)}</ul>
+      <h3>{props.tool.description}</h3>
+      <h4>Description:</h4>
+      <ul>{body.split('*').map(element => <li>{element}</li>)}</ul>
       <Link to={`/confirm/tool/${props.tool.id}`}>
         <Button basic color='green'>Rent</Button>
       </Link>
