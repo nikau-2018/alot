3ff3f0e3a425e458370d87b4eed78d0c32345a0b
james-built <jgbuilt@gmail.com>
2018-09-30 14:12:11 +1300

    in progress on fix

--- client/src/components/WorkshopsContainer/WorkshopsContainer.js
+++ client/src/components/WorkshopsContainer/WorkshopsContainer.js
@@ -13,32 +13,30 @@ export default class WorkshopsContainer extends Component {
 
   componentDidMount () {
     this.props.fetchWorkshops()
-      .then(() => {
+      .then(
         this.setState({selectedId: this.props.categories.find((cat) => (
-          cat.name.toLowerCase() === this.props.match.params.category
-        ))
+          cat.name.toLowerCase() === this.props.match.params.category))
         })
-      })
+      )
     this.props.fetchCategories()
-      .then()
+      .then(
+        this.setState({
+          filteredWorkshops: this.props.workshops.filter((workshop) => (
+            workshop.categoryId === this.state.selectedId))
+        })
+      )
   }
 
   render () {
-    const {id} = this.props.categories.find((cat) => (
-      cat.name.toLowerCase() === this.props.match.params.category
-    ))
-    const filteredWorkshops = this.props.workshops.filter((workshop) => (
-      workshop.categoryId === id
-    ))
     console.log(this.props.workshops)
-    console.log(id)
-    console.log(filteredWorkshops)
+    console.log(this.state.selectedId)
+    console.log(this.state.filteredWorkshops)
     return (
       <div className='workshops-container' >
         {
           this.props.match.params.category
             ? <Workshops
-              workshops={filteredWorkshops}
+              workshops={this.state.filteredWorkshops}
               category={this.props.categories}/>
             : <Workshops
               workshops={this.props.workshops}
