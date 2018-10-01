768d423e89093999ecf8f0d129be85d0190912e6
james-built <jgbuilt@gmail.com>
2018-10-01 19:38:21 +1300

    working with router now instead, branch switch commit

--- client/src/components/AdminNav/index.js
+++ client/src/components/AdminNav/index.js
@@ -5,19 +5,19 @@ import {Button, Icon} from 'semantic-ui-react'
 export default function AdminNav (props) {
   return (
     <div className='ui buttons'>
-      <Button className='ui button' onClick={props.handleClick}>
+      <Button className='ui button' onClick={() => props.navigate('tool')}>
         <Icon name='wrench'/>
         Add Tool
       </Button>
-      <Button className='ui button' onClick={props.handleClick}>
+      <Button className='ui button' onClick={() => props.navigate('workshop')}>
         <Icon name='calendar alternate'/>
         Add Workshop
       </Button>
-      <Button className='ui button' onClick={props.handleClick}>
+      <Button className='ui button' onClick={() => props.navigate('employee')}>
         <Icon name='user plus'/>
         Edit Employee
       </Button>
-      <Button className='ui button' onClick={props.handleClick}>
+      <Button className='ui button' onClick={() => props.navigate('orders')}>
         <Icon name='envelope'/>
         Maintain Orders
       </Button>
