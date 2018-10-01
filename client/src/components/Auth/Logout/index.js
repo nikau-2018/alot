ab29ec85f2e7fa933adaafcc0e3e230c4f733afd
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-10-02 11:30:04 +1300

    Fixed Logout button

--- client/src/components/Auth/Logout/index.js
+++ client/src/components/Auth/Logout/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Button} from 'semantic-ui-react'
+
 import {logoutUser} from './actions'
 
 import styles from './styles.css'
 
 const Logout = (props) => {
   return (
-    <Button className={styles.george} onClick={props.logoutUser}>
-      Logout
-    </Button>
+    <div className={styles.logout}>
+      <Button className={styles.button} onClick={props.logoutUser}>
+        Logout
+      </Button>
+    </div>
   )
 }
 
