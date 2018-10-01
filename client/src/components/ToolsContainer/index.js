052e1a05c2f68a046636116fe2c051cfda6d4565
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-10-01 20:41:50 +1300

    Removed unneccessary line

--- client/src/components/ToolsContainer/index.js
+++ client/src/components/ToolsContainer/index.js
@@ -1,7 +1,6 @@
 import {connect} from 'react-redux'
 
 import {fetchTools} from './actions'
-import {fetchCategories} from '../CategoryFilter/actions'
 
 import ToolsContainer from './ToolsContainer'
 
