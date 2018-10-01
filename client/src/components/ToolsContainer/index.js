f5598917eaa0490464989da697cb128e4857b0e7
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-10-01 20:19:11 +1300

    Removed unneccessary Redux state

--- client/src/components/ToolsContainer/index.js
+++ client/src/components/ToolsContainer/index.js
@@ -8,10 +8,8 @@ import ToolsContainer from './ToolsContainer'
 import styles from './styles.css'
 
 const mapStateToProps = (state) => {
-  const allCategories = state.categories.categories.map((category) => category.id)
   return {
     tools: state.tools.tools,
-    displayedCategory: allCategories,
     search: state.search
   }
 }
