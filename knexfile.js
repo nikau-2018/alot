c999b99d5e01b9dbb1bcb3a9674ed26fe1d10f84
georgehardy <george.hardy@gmail.com>
2018-10-01 20:53:00 +1300

    pgtest

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,7 @@ const wrapIdentifier = (identifier, origImpl) => origImpl(camelToSnake(identifie
 
 module.exports = {
 
-  development: {
+  _development: {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
@@ -38,7 +38,7 @@ module.exports = {
     }
   },
 
-  staging: {
+  development: {
     client: 'postgresql',
     connection: {
       host: '192.168.1.183',
