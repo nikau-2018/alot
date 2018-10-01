e080396afe6fc75589630f9a9bf576be373b81e3
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-10-01 20:23:38 +1300

    Deleted ToolCard commenting from WelcomeContainer

--- client/src/components/WelcomeContainer/WelcomeContainer.js
+++ client/src/components/WelcomeContainer/WelcomeContainer.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react'
-import {Divider} from 'semantic-ui-react'
 
 import Welcome from '../Welcome'
-// import ToolCard from '../ToolCard'
 
 export default class WelcomeContainer extends Component {
   render () {
-    // const randomTool = this.props.tools[Math.floor(Math.random() * this.props.tools.length)]
     return (
       <div className='welcome-container' >
         <Welcome />
-        {/* <Divider />
-        <h3>What a tool...</h3>
-        <ToolCard
-          key={randomTool.id}
-          id={randomTool.id}
-          name={randomTool.name}
-          image={randomTool.image}
-          description={randomTool.description}
-          category={randomTool.categoryId}
-        /> */}
       </div>
     )
   }
