a4167649bbf347d8a88b88784bca778d3d3a3557
Aimee Coxhead <aimeecoxhead1@gmail.com>
2018-10-02 08:28:37 +1300

    working on homepage

---
+++ client/src/components/About/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import {Image, Divider, Grid, Segment} from 'semantic-ui-react'
+
+export default function About () {
+  return (
+    <div>
+      <h2>People</h2>
+      <Grid columns={2}>
+        <Grid.Column>
+          <Segment basic>
+            <h4>Amanada Chapman</h4>
+            <p>info</p>
+            <Image src={`http://www.placepuppy.net/400/250`} />
+          </Segment>
+        </Grid.Column>
+        <Divider vertical></Divider>
+        <Grid.Column>
+          <Segment basic>
+            <h4>Tom Greer</h4>
+            <p>info</p>
+            <Image src={`http://www.placepuppy.net/400/250`} />
+          </Segment>
+        </Grid.Column>
+      </Grid>
+    </div>
+  )
+}
