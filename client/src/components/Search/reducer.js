effc184d978ada57f0261f9dfa3643f4d2a863a0
james-built <jgbuilt@gmail.com>
2018-10-02 14:36:42 +1300

    added searchworkshop reducer, will need to change to named exports

--- client/src/components/Search/reducer.js
+++ client/src/components/Search/reducer.js
@@ -1,9 +1,19 @@
 import {SEARCH_TOOL} from './actions'
+import {SEARCH_WORKSHOP} from './actions'
 
 export default function (state = '', action) {
   switch (action.type) {
     case SEARCH_TOOL:
       return action.searchTool
+    default:
+      return state
+  }
+}
+
+export default function (state = '', action) {
+  switch (action.type) {
+    case SEARCH_WORKSHOP:
+      return action.searchWorkshop
     default:
       return state
   }
