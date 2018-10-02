3999fe0c9191bf6cb05cdd3e50861d9fd26b9d88
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-10-02 16:21:23 +1300

    Removed css from WorkshopsContainer

--- client/src/components/WorkshopsContainer/index.js
+++ client/src/components/WorkshopsContainer/index.js
@@ -4,8 +4,6 @@ import {fetchWorkshops} from './actions'
 
 import WorkshopsContainer from './WorkshopsContainer'
 
-import styles from './styles.css'
-
 const mapStateToProps = (state) => {
   return {
     workshops: state.workshops.workshops,
