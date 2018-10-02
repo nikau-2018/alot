5d3efd0db50e3c010954fbcbf6c484b7154d82a9
james-built <jgbuilt@gmail.com>
2018-10-02 14:07:26 +1300

    trying after typo but still not working

--- client/src/components/EditTool/EditTool.js
+++ client/src/components/EditTool/EditTool.js
@@ -29,7 +29,7 @@ export default class EditTool extends Component {
     axios.defaults.headers.common['Authorization'] = `Bearer ${get('token')}`
     axios
       .put(`/api/v1/tools/edit/${toolId}`, formObj)
-      .then(this.props.go(`/tools/${formObj.categoryId}/${toolId}`))
+      .then(this.props.history.go(`/tools/${formObj.categoryId}/${toolId}`))
       .catch(() => {
         this.toggleError() // need to do proper error handling here eventually
       })
