99cc8915b04fe81e27d24f0d36b0d5875942fa2f
Aimee Coxhead <aimeecoxhead1@gmail.com>
2018-10-03 10:12:30 +1300

    setup commit

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import {Icon} from 'semantic-ui-react'
 
 import styles from './styles.css'
 
 export default function Map () {
   return (
-      <div>
-          <h1>Where are we:</h1>
-          </div>
+    <div>
+      <h1>Find us</h1>
+    </div>
   )
 }
