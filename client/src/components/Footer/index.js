244bb0cd9e4c5cde8caf5ded24f926564698c17c
aimee-coxhead <aimeecoxhead1@gmail.com>
2018-10-02 22:33:48 +1300

    fixed footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -6,9 +6,11 @@ import styles from './styles.css'
 export default function Footer () {
   return (
     <div className={styles.footer}>
-      <h4><Icon name='warehouse' /> Auckland Library of Tools</h4>
+      <h3><Icon name='warehouse' /> Auckland Library of Tools</h3>
       <p><Icon name='location arrow' />Grey Lynn, Auckland</p>
-      <a href="https://www.facebook.com/AucklandLibraryofTools/"><Icon name='facebook' />AucklandLibraryofTools</a><br />
+      <a href="https://www.facebook.com/AucklandLibraryofTools/"><Icon name='facebook' />AucklandLibraryofTools</a>
+      <br />
+      <br />
       <a href="mailto:aucklandtoollibrary@gmail.com"><Icon name='mail' />aucklandtoollibrary@gmail.com</a>
     </div>
   )
