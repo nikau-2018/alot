337598702b3c88523a022d49086b157121459268
aimee-coxhead <aimeecoxhead1@gmail.com>
2018-10-02 22:10:02 +1300

    happy with home page, needs an image

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -10,16 +10,14 @@ export default function About () {
     <Divider />
     <Grid stackable>
         <Grid.Column width={6}>
-        <Image className={styles.image} src={'/alot-logo.png'} />
+        <Image className={styles.image} size='medium'/>
     </Grid.Column>
     <Grid.Column width={10}>
-    <div className={styles.text}>
     <h1 className={styles.h1}>About ALoT</h1>
-        <p className={styles.p}>Auckland Library of Tools (ALoT) is a community hub that provides access to high quality and well maintained tools and equipment.
+        <p>Auckland Library of Tools (ALoT) is a community hub that provides access to high quality and well maintained tools and equipment.
         The hub consists of a well stocked “library”, a storage and repair space and a makerspace where members can attend workshops, learn, collaborate and share
-        resources and ideas. 
-            </p>
-            </div>
+        resources and ideas.</p>
+        <p>The dominant economic model encourages ownership and discourages sharing. Tools rarely used, gather dust. Objects with owners, but no users, fall into disrepair. Overproduction has become the norm, while disused but perfectly functional items slowly make their way to landfill. All the while, people and projects without funds or equipment fail to get off the ground. ALoT offers a better way; one based on the provision of shared resources, and a reduction of the wasteful and widespread communal hoarding of objects. </p>
         </Grid.Column>
     </Grid>
     </div>
