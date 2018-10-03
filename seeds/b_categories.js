d3bfee04d694f8f0d2cdbc92379908681cb9b7fa
tommy-hall <thall2906@gmail.com>
2018-10-03 21:51:17 +1300

    more seed data

--- seeds/b_categories.js
+++ seeds/b_categories.js
@@ -6,7 +6,8 @@ exports.seed = function (knex, Promise) {
         {id: 77702, name: 'Gardening', description: 'Everything to do with gardening', image: '/category-images/gardening.jpg'},
         {id: 77703, name: 'Painting', description: 'Everything to do with painting', image: '/category-images/painting.jpg'},
         {id: 77704, name: 'Woodwork', description: 'Everything to do with woodwork', image: '/category-images/woodwork.jpg'},
-        {id: 77705, name: 'Contruction', description: 'Everything to do with construction', image: '/category-images/construction.jpg'}
+        {id: 77705, name: 'Contruction', description: 'Everything to do with construction', image: '/category-images/construction.jpg'},
+        {id: 77706, name: 'Accessories', description: 'Accessories', image: '/category-images/accessories.jpg'}
       ])
     })
 }
