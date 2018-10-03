054a87bac92ba1d570a742823322507bbe3aa69a
georgehardy <george.hardy@gmail.com>
2018-10-03 21:54:27 +1300

    matching layout of tools

--- client/src/components/Workshops/index.js
+++ client/src/components/Workshops/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Card} from 'semantic-ui-react'
 import Search from '../Search'
 import CategoryFilter from '../CategoryFilter'
 import WorkshopCard from '../WorkshopCard'
@@ -7,7 +7,7 @@ import WorkshopCard from '../WorkshopCard'
 import styles from './styles.css'
 
 const Workshops = (props) => (
-  <div className={styles.tools}>
+  <div className={styles.workshops}>
     <h1 className={styles.header}>Tools</h1>
     <div className={styles.top}>
       <CategoryFilter
@@ -17,7 +17,7 @@ const Workshops = (props) => (
       />
       <Search className={styles.search} toolSearch={false} />
     </div>
-    <div className={styles.toolCards}>
+    <div>
       <Card.Group stackable centered>
         {props.filteredWorkshops.map((workshop) => (
           <WorkshopCard
