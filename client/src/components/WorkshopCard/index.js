fa72e47a242245571db417b6c968c1fd685b53a2
georgehardy <george.hardy@gmail.com>
2018-10-03 21:43:07 +1300

    clone structure from toolcard

--- client/src/components/WorkshopCard/index.js
+++ client/src/components/WorkshopCard/index.js
@@ -2,15 +2,18 @@ import React from 'react'
 import {Card, Image} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
 
-// import styles from './styles.css'
+import styles from './styles.css'
 
 export default function WorkshopCard (props) {
   return (
-    <Card as={Link} to={`/workshops/${props.category}/${props.id}`}>
-      <Card.Content>
-        <Card.Header style={{textAlign: 'center'}}>{props.name}</Card.Header>
-        <Image src={props.image} />
-        <Card.Description>
+    <Card color='purple'
+      as={Link} to={`/workshops/${props.category}/${props.id}`}>
+      <Card.Content className={styles.header}>
+        <Card.Header className={styles.headerText}>{props.name}</Card.Header>
+      </Card.Content>
+      <Image src={props.image} size='medium' fluid centered className={styles.image}/>
+      <Card.Content extra className={styles.content}>
+        <Card.Description className={styles.description}>
           <ul>
             {props.description.split('*').map(element => <li>{element}</li>)}
           </ul>
