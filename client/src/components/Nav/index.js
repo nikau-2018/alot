de0b41e389e33685c24145fd41acc3657b939765
james-built <jgbuilt@gmail.com>
2018-10-04 00:39:16 +1300

    added menu links & positioning

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -18,30 +18,38 @@ class Nav extends Component {
     return (
       <div className={styles.nav}>
         <div className={styles.menuLeft}>
-          <div className={styles.icon} as={Link} to="/">
-            <Icon name='home'/>Home
-          </div>
-
+          <Link to='/'>
+            <div className={styles.icon} as={Link} to="/">
+              <Icon name='home'/>Home
+            </div>
+          </Link>
+          <Link to='/tools'>
           <div className={styles.icon} as={Link} to="/tools">
             <Icon name='wrench' />Tools
           </div>
-
+          </Link>
+          <Link to='/workshops'>
           <div className={styles.icon} as={Link} to="/workshops">
             <Icon name='warehouse'/>Workshops
           </div>
+          </Link>
         </div>
 
         <div className={styles.menuRight} position='right'>
           {this.props.isAdmin &&
-            <div className={styles.icon} as={Link} to="/admin">
-              <Icon name='book'/>Admin
-            </div>
+            <Link to='/admin'>
+              <div className={styles.icon} as={Link} to="/admin">
+                <Icon name='book'/>Admin
+              </div>
+            </Link>
           }
           {this.props.isAuthenticated
             ? <Logout />
-            : <div className={styles.icon} as={Link} to="/login">
-                <Icon name='lock open'/>Login
-              </div>
+            : <Link to='/login'>
+                <div className={styles.icon} as={Link} to="/login">
+                  <Icon name='lock open'/>Login
+                </div>
+              </Link>
           }
         </div>
       </div>
