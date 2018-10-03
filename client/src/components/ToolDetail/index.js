eb6ca0ee6083bf85039cf9659948bd12000ca149
georgehardy <george.hardy@gmail.com>
2018-10-03 21:00:04 +1300

    various tweaks

--- client/src/components/ToolDetail/index.js
+++ client/src/components/ToolDetail/index.js
@@ -11,26 +11,25 @@ export default function ToolDetail (props) {
     <div className={styles.toolDetail}>
       <h1 className={styles.h1}>{props.tool.name}</h1>
       <h3 className={styles.h3}>{props.tool.description}</h3>
-      <Image src={props.tool.image} size='large' centered />
+      <Image bordered className={styles.image}src={props.tool.image} size='large' centered />
       {
         props.tool.available
-          ? <Button as={Link} to={`/confirm/tool/${props.tool.id}`} basic color='green'>Reserve</Button>
-          : <Button basic color='red'>Not available</Button>
+          ? <Button className={styles.button} as={Link} to={`/confirm/tool/${props.tool.id}`} color='green'>Reserve</Button>
+          : <Button className={styles.button} color='red'>Not available</Button>
       }
-      <Button as={Link} to={`/tools/${props.tool.categoryId}`} basic>Similar Tools</Button>
-      {isAdmin && <Button as={Link} to={`/edit/tool/${props.tool.id}`} basic>Edit Tool</Button>}
+      <Button className={styles.button} as={Link} to={`/tools/${props.tool.categoryId}`}>Similar Tools</Button>
+      {isAdmin && <Button className={styles.button} as={Link} to={`/edit/tool/${props.tool.id}`} >Edit Tool</Button>}
       <Grid columns={2} >
         <Grid.Row>
           <Grid.Column>
-            <h5 className={styles.h5one}>In Library: {props.tool.stocked}</h5>
+            <h5 className={styles.h5one}>In Library  {props.tool.stocked}</h5>
           </Grid.Column>
           <Grid.Column>
-            <h5 className={styles.h5two}>Available: {props.tool.available}</h5>
+            <h5 className={styles.h5two}>Available:  {props.tool.available}</h5>
           </Grid.Column>
         </Grid.Row>
       </Grid>
       <Divider/>
-      <h4 className={styles.h4}>Product Description</h4>
       <div className={styles.bullets}>
         <ul>
           {body.split('*').map(element => <li className={styles.li}>{element}</li>)}
