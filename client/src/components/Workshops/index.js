63f0f3a1a11426b961bfc719718813b8e9218ebf
georgehardy <george.hardy@gmail.com>
2018-10-03 21:49:32 +1300

    clone structure from tools

--- client/src/components/Workshops/index.js
+++ client/src/components/Workshops/index.js
@@ -7,24 +7,30 @@ import WorkshopCard from '../WorkshopCard'
 import styles from './styles.css'
 
 const Workshops = (props) => (
-  <div>
-    <Search toolSearch={false} />
-    <CategoryFilter
-      type='workshops'
-      category={props.category}
-    />
-    <div className={styles.workshops}>
-      {props.filteredWorkshops.map((workshop) => (
-        <WorkshopCard
-          category={workshop.categoryId}
-          key={workshop.id}
-          id={workshop.id}
-          name={workshop.name}
-          image={workshop.image}
-          description={workshop.description}
-          instructor={workshop.instructor}
-          dateTime={workshop.dateTime}/>
-      ))}
+  <div className={styles.tools}>
+    <h1 className={styles.header}>Tools</h1>
+    <div className={styles.top}>
+      <CategoryFilter
+        type='workshops'
+        category={props.category}
+        className={styles.filter}
+      />
+      <Search className={styles.search} toolSearch={false} />
+    </div>
+    <div className={styles.toolCards}>
+      <Card.Group stackable centered>
+        {props.filteredWorkshops.map((workshop) => (
+          <WorkshopCard
+            category={workshop.categoryId}
+            key={workshop.id}
+            id={workshop.id}
+            name={workshop.name}
+            image={workshop.image}
+            description={workshop.description}
+            instructor={workshop.instructor}
+            dateTime={workshop.dateTime}/>
+        ))}
+      </Card.Group>
     </div>
   </div>
 )
