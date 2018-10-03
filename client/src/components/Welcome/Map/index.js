c4400481e549305ae5cf12b0b0a72fe36b3a439b
james-built <jgbuilt@gmail.com>
2018-10-03 21:22:31 +1300

    converted the map component

--- client/src/components/Welcome/Map/index.js
+++ client/src/components/Welcome/Map/index.js
@@ -7,23 +7,20 @@ import styles from './styles.css'
 export default function Map () {
   return (
     <div className={styles.map}>
-      <Divider />
-      <Grid stackable>
-        <Grid.Column width={5}>
-          <h2 className={styles.h2}>Address</h2>
-          <p className={styles.p}>11 Example Street</p>
-          <p className={styles.p}>Grey Lynn</p>
-          <p className={styles.p}>Auckland</p>
-        </Grid.Column>
-        <Grid.Column width={5}>
-          <h2 className={styles.h2}>Hours</h2>
-          <p className={styles.p}>Saturday 11am-3pm</p>
-          <p className={styles.p}>Sunday 11am-3pm</p>
-        </Grid.Column>
-        <Grid.Column width={5}>
-          <Image className={styles.image} src={'/grey-lynn.jpg'}size='large'/>
-        </Grid.Column>
-      </Grid>
+      <div>
+        <h2 className={styles.h2}>Address</h2>
+        <p className={styles.p}>11 Example Street</p>
+        <p className={styles.p}>Grey Lynn</p>
+        <p className={styles.p}>Auckland</p>
+      </div>
+      <div>
+        <h2 className={styles.h2}>Hours</h2>
+        <p className={styles.p}>Saturday 11am-3pm</p>
+        <p className={styles.p}>Sunday 11am-3pm</p>
+      </div>
+      <div>
+        <Image className={styles.image} src={'/grey-lynn.jpg'}size='large'/>
+      </div>
     </div>
   )
 }
