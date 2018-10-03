86d586e5f0df411047117ea1da12c9be0b44cf30
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-10-03 17:41:58 +1300

    Added email links to Meet the Team

--- client/src/components/Welcome/People/index.js
+++ client/src/components/Welcome/People/index.js
@@ -15,17 +15,18 @@ export default function People () {
             <Image className={styles.image} src={'/people/amanda.jpg'} circular/>
             <p className={styles.p}>Amanda Chapman​ established the Auckland Community Fridge, NZ’s first free food sharing fridge. She blogs       about waste reduction and her tiny house build. Amanda works for We Compost and United Sustainable Sisters
               (part of Tamaki WRAP). She has completed a PGDipSci in Environmental Science and Geography.</p>
-            <p className={styles.email} ><Icon name='mail' />amandawastefree@gmail.com</p>
+            <a href="mailto:amandawastefree@gmail.com" className={styles.email}>
+              <Icon name='mail' /> amandawastefree@gmail.com
+            </a>
           </Grid.Column>
           <Grid.Column>
-            <h2>Tom Greer</h2>
+            <h2 className={styles.h2}>Tom Greer</h2>
             <Image className={styles.image} src={'/people/tomGreer.jpg'} circular/>
             <p className={styles.p}>Tom Greer​ studies Landscape Architecture, and has worked in ecology and environmental science. He has a
               passion for the natural world, design and social systems. Tom previously studied biology and statistics,
               and has completed a BA(Hons) and PGDipSci. He also has several years experience working in libraries of
               books (libraries)</p>
-            <p className={styles.email}><Icon name='mail' />tza.greer@gmail.com</p>
-            <a href="mailto:tza.greer@gmail.com">
+            <a href="mailto:tza.greer@gmail.com" className={styles.email}>
               <Icon name='mail' /> tza.greer@gmail.com
             </a>
           </Grid.Column>
