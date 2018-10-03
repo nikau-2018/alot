1f5417c025444bf7afc38388ca149d5ff1a91bf8
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-10-03 14:43:27 +1300

    Tidied register css

--- client/src/components/Auth/Register/RegisterForm.jsx
+++ client/src/components/Auth/Register/RegisterForm.jsx
@@ -46,68 +46,70 @@ export default class RegisterForm extends React.Component {
     return (
       <div className={styles.registerForm}>
         <Form>
-          <h1>SIGN UP</h1>
-          <div className={styles.inputs}>
-            <Form.Input transparent
-              name='firstName'
-              placeholder='First Name'
-              value={firstName}            
-              onChange={this.handleChange}
-              className={styles.input}
-              icon='user'
-              iconPosition='left'
-            />
-            <Form.Input transparent
-              name='lastName'
-              placeholder='Last Name'
-              value={lastName}            
-              onChange={this.handleChange}
-              className={styles.input}
-              icon='user'
-              iconPosition='left'
-            />
-            <Form.Input transparent
-              name='email'
-              placeholder='Email'
-              value={email}
-              onChange={this.handleChange}
-              className={styles.input}
-              icon='mail'
-              iconPosition='left'
-            />
-            <Form.Input transparent 
-              name='phone'
-              placeholder='Phone Number'
-              value={phone}
-              onChange={this.handleChange}
-              className={styles.input}
-              icon='phone'
-              iconPosition='left'
-            />
-            <Form.Input transparent
-              name='password'
-              type='password'
-              placeholder='********'
-              value={password}            
-              onChange={this.handleChange}
-              className={styles.input}
-              icon='lock'
-              iconPosition='left'
-            />
-            <Form.Input transparent
-              name='confirm'
-              type='password'
-              placeholder='********'
-              value={confirm}            
-              onChange={this.handleChange}
-              className={styles.input}
-              icon='lock'
-              iconPosition='left'
-            />
+          <div className={styles.register}>          
+            <h1>SIGN UP</h1>
+            <div className={styles.inputs}>
+              <Form.Input transparent
+                name='firstName'
+                placeholder='First Name'
+                value={firstName}            
+                onChange={this.handleChange}
+                className={styles.input}
+                icon='user'
+                iconPosition='left'
+              />
+              <Form.Input transparent
+                name='lastName'
+                placeholder='Last Name'
+                value={lastName}            
+                onChange={this.handleChange}
+                className={styles.input}
+                icon='user'
+                iconPosition='left'
+              />
+              <Form.Input transparent
+                name='email'
+                placeholder='Email'
+                value={email}
+                onChange={this.handleChange}
+                className={styles.input}
+                icon='mail'
+                iconPosition='left'
+              />
+              <Form.Input transparent 
+                name='phone'
+                placeholder='Phone Number'
+                value={phone}
+                onChange={this.handleChange}
+                className={styles.input}
+                icon='phone'
+                iconPosition='left'
+              />
+              <Form.Input transparent
+                name='password'
+                type='password'
+                placeholder='********'
+                value={password}            
+                onChange={this.handleChange}
+                className={styles.input}
+                icon='lock'
+                iconPosition='left'
+              />
+              <Form.Input transparent
+                name='confirm'
+                type='password'
+                placeholder='********'
+                value={confirm}            
+                onChange={this.handleChange}
+                className={styles.input}
+                icon='lock'
+                iconPosition='left'
+              />
+            </div>
+            <Button onClick={this.handleClick} className={styles.button}>SIGN UP</Button><br />            
           </div>
           <ErrorMessage reducer='auth' />
-          <Button onClick={this.handleClick} className={styles.button}>SIGN UP</Button><br />
-          <div className={styles.p}>
+          <div>
             Already have an account? <Button onClick={this.handleSwitch} className={styles.switch}>Login</Button>
           </div>
       </Form>
