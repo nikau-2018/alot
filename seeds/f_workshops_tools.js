b8dac4ff08e55051c521b150911d9248c360ea5a
georgehardy <george.hardy@gmail.com>
2018-10-04 10:10:36 +1300

    fix missing tool

--- seeds/f_workshops_tools.js
+++ seeds/f_workshops_tools.js
@@ -4,7 +4,7 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       return knex('workshops_tools').insert([
         {id: 99901, workshop_id: 88801, tool_id: 55503},
-        {id: 99902, workshop_id: 88802, tool_id: 55505},
+        {id: 99902, workshop_id: 88803, tool_id: 55505},
         {id: 99903, workshop_id: 88803, tool_id: 55504}
       ])
     })
